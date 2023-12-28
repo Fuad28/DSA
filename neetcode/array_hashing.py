166bc7d92614f9286551bd38ef83861948d84260
Fuad <fuadadio75@gmail.com>
2023-12-28 22:33:40 +0100

    .

--- neetcode/array_hashing.py
+++ neetcode/array_hashing.py
@@ -1,46 +1,50 @@
-# from typing import List
+from typing import List
 
 
-# # Contains Duplicate: https://leetcode.com/problems/contains-duplicate/
+# Contains Duplicate: https://leetcode.com/problems/contains-duplicate/
 
-# class Solution:  # time: O(n) space: O(n)
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         seen = set()
+class Solution:  # time: O(n) space: O(n)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
 
-#         for num in nums:
-#             if num in seen:
-#                 return True
+        for num in nums:
+            if num in seen:
+                return True
 
-#             seen.add(num)
+            seen.add(num)
 
-#         return False
-
-
-# # valid anagram: https://leetcode.com/problems/valid-anagram/
-
-# # solution 1
-# class Solution:  # time: O(nlogn) space: O(1)
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         return sorted(s) == sorted(t)
+        return False
 
 
-# # two sum: https://leetcode.com/problems/two-sum/
+# valid anagram: https://leetcode.com/problems/valid-anagram/
 
-# # solution 1
-# class Solution:  # time: O(n^2) space: O(1)
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i in range(len(nums)):
-#             for j in range(len(nums)):
-#                 if (i != j) and (nums[i] + nums[j] == target):
-#                     return [i, j]
+# solution 1
+class Solution:  # time: O(nlogn) space: O(1)
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
 
-nums = [3, 3]
-target = 6
 
-nums_dict = {i: j for i, j in enumerate(nums)}
-print(nums_dict)
-for j in nums_dict:
-    i = target - j
-    print(nums_dict[j], nums_dict[i])
-    if (i in nums_dict) and (nums_dict[j] != nums_dict[i]):
-        print(nums_dict[j], nums_dict[i])
+# two sum: https://leetcode.com/problems/two-sum/
+
+# solution 1
+class Solution:  # time: O(n^2) space: O(1)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (i != j) and (nums[i] + nums[j] == target):
+                    return [i, j]
+
+# solution 2
+
+
+class Solution:  # time: O(n) space: O(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+
+        for idx, j in enumerate(nums):
+            i = target - j
+
+            if (i in nums_dict):
+                return [idx, nums_dict[i]]
+
+            nums_dict[j] = idx
