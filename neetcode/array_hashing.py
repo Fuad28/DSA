bec3dfc52e1f7b3836eb9c3fc7a9e6bf80d3beea
Fuad <fuadadio75@gmail.com>
2023-12-29 22:31:06 +0100

    added group anagram solution

--- neetcode/array_hashing.py
+++ neetcode/array_hashing.py
@@ -1,7 +1,8 @@
 from typing import List
+from collections import defaultdict
 
 
-# Contains Duplicate: https://leetcode.com/problems/contains-duplicate/
+# Contains Duplicate: https://leetcode.com/problems/contains-duplicate/ Easy
 
 class Solution:  # time: O(n) space: O(n)
     def containsDuplicate(self, nums: List[int]) -> bool:
@@ -16,7 +17,7 @@ class Solution:  # time: O(n) space: O(n)
         return False
 
 
-# valid anagram: https://leetcode.com/problems/valid-anagram/
+# valid anagram: https://leetcode.com/problems/valid-anagram/ Easy
 
 # solution 1
 class Solution:  # time: O(nlogn) space: O(1)
@@ -24,7 +25,7 @@ class Solution:  # time: O(nlogn) space: O(1)
         return sorted(s) == sorted(t)
 
 
-# two sum: https://leetcode.com/problems/two-sum/
+# two sum: https://leetcode.com/problems/two-sum/ Easy
 
 # solution 1
 class Solution:  # time: O(n^2) space: O(1)
@@ -48,3 +49,32 @@ class Solution:  # time: O(n) space: O(n)
                 return [idx, nums_dict[i]]
 
             nums_dict[j] = idx
+
+
+# Group Anagrams: https://leetcode.com/problems/group-anagrams/ Medium
+
+# solution 1
+class Solution:  # time: O(mnlogn) space: O(n)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output_dict = defaultdict(list)
+
+        for word in strs:
+            output_dict["".join(sorted(word))].append(word)
+
+        return output_dict.values()
+
+
+# solution 2
+class Solution:  # time: O(mn) space: O(n)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+
+            for char in word:
+                count[ord(char) - ord("a")] += 1
+
+            res[tuple(count)].append(word)
+
+        return res.values()
