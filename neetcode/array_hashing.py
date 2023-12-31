4f32175d25ac0e8087d66ddf0381f7b603a725b5
Fuad <fuadadio75@gmail.com>
2023-12-31 23:26:52 +0100

    added solution to topKFrequent

--- neetcode/array_hashing.py
+++ neetcode/array_hashing.py
@@ -78,3 +78,27 @@ class Solution:  # time: O(mn) space: O(n)
             res[tuple(count)].append(word)
 
         return res.values()
+
+
+# Top K Frequent Elements: https://leetcode.com/problems/top-k-frequent-elements/ Medium
+
+class Solution:  # time: O(n) space: O(n)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mapper = defaultdict(self.zero)
+        freq_map = {count: [] for count in range(1, len(nums) + 1)}
+        output_arr = []
+
+        for num in nums:
+            mapper[num] += 1
+
+        for num in mapper:
+            freq_map[mapper[num]].append(num)
+
+        for i in range(len(nums), -1, -1):
+            if len(output_arr) != k:
+                output_arr.extend(freq_map[i])
+
+        return output_arr
+
+    def zero(self):
+        return 0
