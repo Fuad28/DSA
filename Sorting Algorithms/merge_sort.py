1d9bf9f64a52c2627b3e22ca912d38843479ee56
Fuad <fuadadio75@gmail.com>
2024-01-02 12:39:29 +0100

    finishd merge sort

--- Sorting Algorithms/merge_sort.py
+++ Sorting Algorithms/merge_sort.py
@@ -1,6 +1,5 @@
 
-def merge_sort(arr: list):
-    came_in = arr.copy()
+def merge_sort(arr: list):  # time: O(nlogn) space: O(n)
 
     if len(arr) <= 1:
         return arr
@@ -11,6 +10,8 @@ def merge_sort(arr: list):
     # Create left_arr ← arr[start..mid] and right_arr ← arr[mid+1..end]
     left_arr = arr[:mid_idx]
     right_arr = arr[mid_idx:]
+
+    print(f"arr_in: {arr} -- left_in: {left_arr} -- right_in: {right_arr}")
 
     # sort the two halves
     left = merge_sort(left_arr)
@@ -39,13 +40,12 @@ def merge_sort(arr: list):
         i += 1
         k += 1
 
-    while i < len(left):
-        arr[k] = left[i]
-        i += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
         k += 1
 
-    print(
-        f"arr_came_in: {came_in} -- arr: {arr} ---- left: {left} --- right: {right}")
+    print(f"left_out: {left} -- right_out: {right} -- arr_out: {arr}\n\n")
 
     return arr
 
@@ -53,19 +53,40 @@ def merge_sort(arr: list):
 print(merge_sort([5, 9, 3, 1, 2, 8, 4, 7, 6]))
 
 """
-
-arr: [5, 9] ---- left: [5] --- right: [9]
-arr: [1, 3] ---- left: [3] --- right: [1]
-arr: [1, 3, 5, 9] ---- left: [5, 9] --- right: [1, 3]
-arr: [2, 8] ---- left: [2] --- right: [8]
-arr: [6, 7] ---- left: [7] --- right: [6]
-arr: [4, 7, 6] ---- left: [4] --- right: [6, 7]
-arr: [2, 4, 7, 6, 8] ---- left: [2, 8] --- right: [4, 7, 6]
-arr: [1, 2, 3, 4, 5, 7, 6, 8, 9] ---- left: [1, 3, 5, 9] --- right: [2, 4, 7, 6, 8]
+To better undertsand how the sorting was carried out.
 
 
+arr_in: [5, 9, 3, 1, 2, 8, 4, 7, 6] -- left_in: [5, 9, 3, 1] -- right_in: [2, 8, 4, 7, 6]
+arr_in: [5, 9, 3, 1] -- left_in: [5, 9] -- right_in: [3, 1]
+arr_in: [5, 9] -- left_in: [5] -- right_in: [9]
+left_out: [5] -- right_out: [9] -- arr_out: [5, 9]
 
 
-[1, 2, 3, 4, 5, 7, 6, 8, 9]
+arr_in: [3, 1] -- left_in: [3] -- right_in: [1]
+left_out: [3] -- right_out: [1] -- arr_out: [1, 3]
 
+
+left_out: [5, 9] -- right_out: [1, 3] -- arr_out: [1, 3, 5, 9]
+
+
+arr_in: [2, 8, 4, 7, 6] -- left_in: [2, 8] -- right_in: [4, 7, 6]
+arr_in: [2, 8] -- left_in: [2] -- right_in: [8]
+left_out: [2] -- right_out: [8] -- arr_out: [2, 8]
+
+
+arr_in: [4, 7, 6] -- left_in: [4] -- right_in: [7, 6]
+arr_in: [7, 6] -- left_in: [7] -- right_in: [6]
+left_out: [7] -- right_out: [6] -- arr_out: [6, 7]
+
+
+left_out: [4] -- right_out: [6, 7] -- arr_out: [4, 6, 7]
+
+
+left_out: [2, 8] -- right_out: [4, 6, 7] -- arr_out: [2, 4, 6, 7, 8]
+
+
+left_out: [1, 3, 5, 9] -- right_out: [2, 4, 6, 7, 8] -- arr_out: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
 """
