014144e90b204c78ea707a3519956f2214b09171
Fuad <fuadadio75@gmail.com>
2024-03-14 16:15:40 +0100

    feat: adds palindromic substring (GS hackerrank)

--- neetcode/two_pointers.py
+++ neetcode/two_pointers.py
@@ -115,4 +115,28 @@ class Solution:# time: O(n), space: O(1)
         
         return max_area
 
+
+# Palindromic Substrings: https://leetcode.com/problems/palindromic-substrings/description/
+class Solution: # time: O(n^2), space: O(1)
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        
+        for i in range(len(s)):
+            l = r = i
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            
+            l = i
+            r = i + 1
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        
+        return count
+
         
